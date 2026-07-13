3a9978f7393c3e952821991c61f3ea145b922a8f
Abrar <araha002@gold.ac.uk>
2026-07-13 14:34:16 +0100

    Updated Requirements and Simple API Call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import requests
+import pprint as pp
 from dotenv import load_dotenv
 
 # Load environment api key from .env file
@@ -7,3 +9,22 @@ api_key = os.getenv("API_KEY")
 
 BASE_URL = "https://api.rawg.io/api/games"
 
+params = {
+    "key": api_key,
+    "page_size": 1
+}
+
+try:
+    response = requests.get(BASE_URL, params=params)
+    
+    response.raise_for_status()
+    
+    data = response.json()
+
+    #print(f"Found {len(data['results'])} games:\n")
+    pp.pprint(data['results'])
+
+except requests.exceptions.HTTPError as err:
+    print(f"HTTP Error occurred: {err}")
+except Exception as err:
+    print(f"An error occurred: {err}")
